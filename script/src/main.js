62f87efca87aea6334a9f04a670eea8e2f6601eb
Peter Plevko <peter.plevko@team.blue>
2026-04-16 11:44:24 +0200

    add div

--- script/src/main.js
+++ script/src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+
+// Create #app div automatically if it doesn't exist
+let appEl = document.getElementById('app')
+if (!appEl) {
+  appEl = document.createElement('div')
+  appEl.id = 'app'
+  document.body.appendChild(appEl)
+}
 
 createApp(App).mount('#app')
